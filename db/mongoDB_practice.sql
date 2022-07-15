d9bad2a0089eb4e1e60221a8cb8d68fe197a1482
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-07-15 12:06:58 +0530

    Update mongoDB_practice.sql

--- db/mongoDB_practice.sql
+++ db/mongoDB_practice.sql
@@ -7,3 +7,7 @@ use DATABASE_NAME;
 db.hex2db.insert({name:"uma"}) ;
 db.dropdatabase;
 
+ db.createCollection('food' );
+ show collections;
+ db.createCollection('vacation' );
+ db.food.insert({name:"briyani",level:"highly spicy"})
