607b7753272784e9f13205785b638e5bc2c231ef
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-07-19 10:47:07 +0530

    Add files via upload

---
+++ java/src/Hello.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Hello {
+	public static void main( String[] args ){
+		System.out.println(2+ "5" );
+	
+	}
+}
---
+++ java/src/day1/DataTypeDemo.java
@@ -0,0 +1,71 @@
+package src.day1;
+
+
+
+public class DataTypeDemo {
+    public static void main( String[] args ){
+
+        // biscuit cream/parleg = 20;
+        // System.out.println("Hello Team");
+        int iceCream = 50;
+        // int ->datatype
+        // iceCream -> variable Name
+        // = -> assigning
+        // 20 -> value 
+
+
+        System.out.println("Goodmorning");
+        System.out.println("20");
+        System.out.println(iceCream);
+        System.out.println("iceCream");
+        int biscuit ;
+        biscuit = 10;
+        System.out.println(biscuit);
+        int pizza ;
+        pizza  = 50;
+        System.out.println(pizza);
+        pizza  = 7;
+        System.out.println("pizza");
+        System.out.println(pizza); 
+
+        int burger = 40;
+        int Burger = 45;
+        int bUrger = 50;
+        int buRgeR = 20;
+
+        System.out.println("Burger");
+        System.out.println(Burger); 
+
+        byte   count = 25;
+        System.out.println(count); 
+short a = 5;
+System.out.println(a); 
+long b = 45L;
+System.out.println(b); 
+
+
+float c = 100.45f;
+
+
+
+double e = 45.879798792d;
+
+
+
+boolean isDataType = false;
+System.out.println(isDataType);
+
+// int g = 10;
+// int g = 50;
+// System.out.println(g);   duplicate local variable not allowed
+
+
+char alpha = 't';
+System.out.println(alpha);  //always give in single quotes 
+
+
+int fries = 100;
+byte sweet = 100;
+
+    }
+}
