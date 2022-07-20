5d5cb8c7fded032228db4ebdb4d0b2493c29da4d
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-07-20 15:16:05 +0530

    Add files via upload

--- java/src/day2/ForLoopDemo.java
+++ java/src/day2/ForLoopDemo.java
@@ -3,10 +3,14 @@ package src.day2;
 public class ForLoopDemo {
     public static void main(String[] args) {
 
-        for(int i = 5; i>=0; i--){    
-           System.out.println(i);    
-            } 
+        // for(int i = 5; i>=0; i--){
+        // System.out.println(i);
+        // }
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("$");
+            }
+            System.out.println();
+        }
     }
 }
-   
-    
