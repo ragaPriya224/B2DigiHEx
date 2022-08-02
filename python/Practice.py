217d1563ac2b7d3c3ead57854e2ce77b2b946959
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2022-08-02 10:24:20 +0530

    python practice

---
+++ python/Practice.py
@@ -0,0 +1,59 @@
+# print("welcome to python world");
+# print("Break time");
+num = 2.5
+type(num)
+print(type(num))
+name = "user"
+name = 25;
+print(type(name))
+mark = 2+3j;
+print(type(mark))
+
+role = 'dev'
+print(type(role))
+job = '''tester'''
+
+print(type(job))
+
+planet = "earth"
+
+print(planet +"world")
+print("python"*5)
+
+data=[22,33,44,55]
+print(data)  #list example->ordered
+print(type(data))
+print(data[0])
+print("magic")
+
+project=["BI","ATM","BFS"];
+Employee = [12,'coding',3333]
+value=[Employee,project]
+print(value)
+print('magic')
+
+
+tup= (12,33,44,55)
+print(tup)
+
+s={21,71,1,51}
+print(s) #set example ->not maintain order
+
+id = {1,3,1,3,5}
+print(id)   #set example-> no repitions
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  5: 1964
+}
+print(thisdict)  #dictionary example
+
+print(thisdict["model"])
+print(thisdict[5])
+a = 22;
+b = 11;
+comp=a>b
+print(comp)
+print(type(comp))
